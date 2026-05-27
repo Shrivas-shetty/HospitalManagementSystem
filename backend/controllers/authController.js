05bb20f80ecfb03a66ffa88154a9affdd933bde3
Shrivas-shetty <shrivasshetty2006@gmail.com>
2026-05-28 01:31:37 +0530

    Error message proper display during login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,7 +16,7 @@ exports.login = (req, res) => {
   const { username, password } = req.body;
   const sql = "SELECT * FROM users WHERE username = ?";
   db.query(sql, [username], async (err, result) => {
-    if (err) return res.status(500).send("Error");
+    if (err) return res.status(500).json(err);
     if (result.length === 0) {
       return res.status(401).send("User not found");
     }
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://hospitalmanagementsystem-owh0.onrender.com"
 });
 
 // attach token automatically
