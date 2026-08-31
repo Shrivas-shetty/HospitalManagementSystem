55711a746fd1a9a3252a16931a39e60b5a4e3718
Shrivas-shetty <shrivasshetty2006@gmail.com>
2026-09-01 00:58:37 +0530

    Changes production Db to Aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,25 @@
-
 require('dotenv').config();
 
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,             //host,user,password,name,port are pulled from railway given credentials
-  user: process.env.DB_USER,             //its there in .env also locally just for reference
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dateStrings: true
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dateStrings: true,
+    ssl: {
+        ca: process.env.DB_CA
+    }
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error('DB connection failed:', err);
-  } else {
-    console.log('Connected to Railway MySQL ');
-  }
+    if (err) {
+        console.error('DB connection failed:', err);
+    } else {
+        console.log('Connected to Aiven MySQL');
+    }
 });
 
 module.exports = db;
